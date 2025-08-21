05ca1456928ea2d0e4948ffdd0442ec8ed294f02
Le Minh Phuong <phuongleminh841@gmail.com>
2025-08-21 15:47:17 +0700

    Feat(): - Add dabase connection. - Consider to update database(error when Update-Database).

---
+++ Models/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace SocialMediaPlatformBackend.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int ProfileId { get; set; }
+        public Profile Profile { get; set; }
+    }
+}
--- Profile.cs
+++ Profile.cs
@@ -1,8 +1,7 @@
 ﻿public class Profile
 {
-
+    public int ProfileId { get; set; }
     public string Name { get; set; }
-
     public string Avatar { get; set; }
     public DateTime CreateDate { get; set; }
 }
