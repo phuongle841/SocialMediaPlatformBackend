c0c9c9a249957201e4d1d1e8c5c9de90321e95af
Le Minh Phuong <phuongleminh841@gmail.com>
2025-08-21 15:47:17 +0700

    Feat(): - Add dabase connection. - Consider to update database(error when Update-Database).

---
+++ Data/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaPlatformBackend.Models;
+
+namespace SocialMediaPlatformBackend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SocialMediaPlatformBackend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,11 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Configuration.AddUserSecrets<Program>(optional: true);
-
-string? secretValue = builder.Configuration["username"];
-Console.WriteLine($"Secret Value: {secretValue}");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
